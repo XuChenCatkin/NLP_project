288da133666332f2ac4f37ed0ba6d01b620e27bc
Xiang <xiangzhang350@gmail.com>
2025-04-02 20:19:23 +0100

    change the wandb singin

--- retrieval_finetune_test.py
+++ retrieval_finetune_test.py
@@ -96,7 +96,7 @@ args = {
     "hard_multi_file": HARD_M,
     "batch_size": 16,
     "huggingfaceusername": "CatkinChen",
-    "wandbusername": "aaron-cui990810-ucl",
+    "wandbusername": "xiangzhang350-ucl",
     "epochs": 5,
     "margin": 0.3,
     "test_size": 0.2,
