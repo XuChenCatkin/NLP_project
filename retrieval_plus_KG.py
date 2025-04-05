0651aacef939a304ba71659623d4c9e1de40cea2
kun <zhaokunxuu@gmail.com>
2025-04-05 18:11:28 +0100

    kg and relation_rank_index

--- retrieval_plus_KG.py
+++ retrieval_plus_KG.py
@@ -142,67 +142,55 @@ def KG_dense_retrieval(queries, all_queries_list, sub_queries_index, faiss_index
     results = []
     kg_ids = []
     for query in queries:
-        entities = process_gpt(query)
-        shared_kg = find_chunk_id(entities)
-        kg_ids.extend(shared_kg)
-    kg_ids = list(set(kg_ids))
-    for query in queries:
         #query_emb = model.encode(query, convert_to_numpy=True, normalize_embeddings=True)
-
-        sel_kg = faiss.IDSelectorArray(kg_ids)
-        params = faiss.SearchParameters()
-        params.sel = sel_kg
         
         query_emb = query_embed_search(query, all_queries_list, sub_queries_index)
         query_emb = query_emb.reshape(1, -1)  # Reshapes to (1, d)
-
-        distances, indices = faiss_index_kg.search(query_emb,1000,params=params)
+        distances, indices = faiss_index_kg.search(query_emb,1000)
+        entities = process_gpt(query)
+        shared_kg = find_chunk_id(entities)
+        print(shared_kg)
         relation_rank_index = indices[0]
+        print(relation_rank_index)
         for i in relation_rank_index:
             if relation_to_kgid_map[i] in shared_kg:
                 kg_ids.append(relation_to_kgid_map[i])
-    if len(kg_ids) == 0:
-        print('KG method failed')
-        print('return the dense retrieval')
-        return dense_retrieval_subqueries_for_finetune(queries, all_queries_list, sub_queries_index, faiss_index_chunk, all_chucks, top_k=5)
-    else:
-        chunk_id_list = []
-        for kg_id in kg_ids:
-            chunk_id_list.extend([i for i in range(5*kg_id-4, 5*kg_id+1)])
-            chunk_id_list = list(set(chunk_id_list))
-        # print(chunk_id_list)
-        sel = faiss.IDSelectorArray(chunk_id_list)
-        params = faiss.SearchParameters()
-        params.sel = sel
-        return dense_retrieval_subqueries_for_finetune(queries, all_queries_list, sub_queries_index, faiss_index_chunk, all_chucks, top_k=5,params=params)
+        if len(kg_ids) == 0:
+            print('KG method failed')
+            print('return the dense retrieval')
+            return dense_retrieval_subqueries_for_finetune(queries, all_queries_list, sub_queries_index, faiss_index_chunk, all_chucks, top_k=5)
+        else:
+            chunk_id_list = []
+            for kg_id in kg_ids:
+                chunk_id_list.extend([i for i in range(5*kg_id, 5*kg_id+5)])
+                chunk_id_list = list(set(chunk_id_list))
+            sel = faiss.IDSelectorArray(chunk_id_list)
+            params = faiss.SearchParameters()
+            params.sel = sel
+            return dense_retrieval_subqueries_for_finetune(queries, all_queries_list, sub_queries_index, faiss_index_chunk, all_chucks, top_k=5,params=params)
 
 if __name__ == "__main__":
-    data =   {
-        "question": "What magical plant traps Harry, Ron, and Hermione in their first year?",
-        "answer": "Devil's Snare",
+    data =  {
+        "question": "What spell does Harry use to disarm Draco Malfoy in the Room of Requirement?",
+        "answer": "Expelliarmus",
         "list of reference": [
             {
-                "ref_id": 643,
-                "passage": "Hermione had managed to free herself before the plant got a firm grip on her. Now she watched in horror as the two boys fought to pull the plant off them, but the more they strained against it, the tighter and faster the plant wound around them. \"Stop moving!\" Hermione ordered them. \"I know what this is - it's Devil's Snare!\" \"Oh, I'm so glad we know what it's called, that's a great help,\" snarled Ron, leaning back, trying to stop the plant from curling around his neck.",
-                "book": 1,
-                "chapter": 16
-            },
-            {
-                "ref_id": 644,
-                "passage": "\"Shut up, I'm trying to remember how to kill it!\" said Hermione. \"Well, hurry up, I can't breathe!\" Harry gasped, wrestling with it as it curled around his chest. \"Devil's Snare, Devil's Snare ... what did Professor Sprout say? - it likes the dark and the damp -\"\n\"So light a fire!\" Harry choked. \"Yes - of course - but there's no wood!\" Hermione cried, wringing her hands. \"HAVE YOU GONE MAD?\"",
-                "book": 1,
-                "chapter": 16
+                "ref_id": 8976,
+                "passage": "\"Don't kill him! DON'T KILL HIM!\" Malfoy yelled at Crabbe and Goyle, who were both aiming at Harry: Their split second's hesitation was all Harry needed. \"Expelliarmus!\" Goyle's wand flew out of his hand and disappeared into the bulwark of objects beside him; Goyle leapt foolishly on the spot, trying to retrieve it; Malfoy jumped out of range of Hermione's second Stunning Spell, and Ron, appearing suddenly at the end of the aisle, shot a full Body-Bind Curse at Crabbe, which narrowly missed. Crabbe wheeled around and screamed, \"Avada Kedavra!\" again. Ron leapt out of sight to avoid the jet of green light. The wandless Malfoy cowered behind a three-legged wardrobe as Hermione charged toward them, hitting Goyle with a Stunning Spell as she came.",
+                "book": 7,
+                "chapter": 31
             }
         ],
-        "id": 9,
-        "question_variants": "What enchanted flora ensnares Harry, Ron, and Hermione during their initial year at Hogwarts?",
+        "id": 1,
+        "question_variants": "Which enchantment did Harry employ to neutralize Draco Malfoy's spell in the Room of Requirement?",
         "sub_questions": [
-            "What is the name of the magical plant that traps the trio in their first year?",
-            "How does this plant specifically trap Harry, Ron, and Hermione?",
-            "Are there any other magical plants or creatures mentioned in the story that could have been used instead of the one that trapped them?"
+            "What is the name of the spell Harry uses to disarm Draco Malfoy?",
+            "How does Harry's spell work in the context of the Room of Requirement?",
+            "Is this spell a defensive or offensive incantation?"
         ],
         "category": "medium_single_labeled"
     }
+    CATEGORY = data["category"]
     EMBEDDING_PATH = "./embedding"
     DATA_PATH = "./data"
     KG_PATH = f"{DATA_PATH}/KG_result_cleaned.json"
@@ -215,20 +203,17 @@ if __name__ == "__main__":
             relation = relation.replace("|", " ")
             relation_to_kgid_map.append(i+1)
 
-    EASY_INDEX = faiss.read_index(f"embedding/BAAI/bge-base-en-v1.5_finetuned/medium_single_labeled_embeddings.index")
-    EASY_ALL_SUB = retrieve_all_subqueries(f"{DATA_PATH}/QA_set/medium_single_labeled.json")
+    EASY_INDEX = faiss.read_index(f"embedding/BAAI/bge-base-en-v1.5_finetuned/{CATEGORY}_embeddings.index")
+    EASY_ALL_SUB = retrieve_all_subqueries(f"{DATA_PATH}/QA_set/{CATEGORY}.json")
     CORPUS_EMBEDDING = faiss.read_index('embedding/BAAI/bge-base-en-v1.5_finetuned/hp_all_BAAI/bge-base-en-v1.5_finetuned.index')
     KG_EMBEDDING = faiss.read_index('embedding/BAAI/bge-base-en-v1.5_finetuned/hp_all_BAAI/bge-base-en-v1.5_finetuned.index')
     CORPUS_FILE = f"{DATA_PATH}/chunked_text_all_together_cleaned.json"
     with open(CORPUS_FILE, 'r') as f:
         CORPUS_DATA = json.load(f)
     # result = dense_retrieval_subqueries_for_finetune(data['sub_questions'], EASY_ALL_SUB, EASY_INDEX, CORPUS_EMBEDDING,CORPUS_DATA , top_k=5)
-    # print(type(data))
     result = KG_dense_retrieval(data['sub_questions'], EASY_ALL_SUB, EASY_INDEX, KG_EMBEDDING, CORPUS_EMBEDDING, CORPUS_DATA, relation_to_kgid_map, top_k=5)
     result_1 = dense_retrieval_subqueries_for_finetune(data['sub_questions'], EASY_ALL_SUB, EASY_INDEX, CORPUS_EMBEDDING,CORPUS_DATA , top_k=5)
-    print(result)
-    print('----------------')
-    print('----------------')
-    print('----------------')
-    print('----------------')
-    print(result_1)
+    result_chunk_ids = [item['chunk_id'] for item in result]
+    result1_chunk_ids = [item['chunk_id'] for item in result_1]
+    print("kg+retrieval: ", result_chunk_ids)
+    print("retrieval: ", result1_chunk_ids)
