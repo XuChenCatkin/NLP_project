366a68be9657f4e26f2f2c4abc090c9c840987bd
xiang-a-xiang <xiangzhang350@gmail.com>
2025-04-05 21:34:45 +0800

    hand code the loss function for finetune

--- retrieval_finetune_test.py
+++ retrieval_finetune_test.py
@@ -94,7 +94,7 @@ args = {
     "medium_multi_file": MEDIUM_M,
     "hard_single_file": HARD_S,
     "hard_multi_file": HARD_M,
-    "batch_size": 30,
+    "batch_size": 3,
     "huggingfaceusername": "CatkinChen",
     "wandbusername": "xiangzhang350-ucl",
     "epochs": 5,
@@ -145,10 +145,10 @@ def hard_negative_mining(item):
         sub_questions = item["sub_questions"]
         index_file, all_subquestion_list = load_index_and_all_subqueries(item["category"])
         negative_retrieval_set = set()
-        top_20_retrieval = dense_retrieval_subqueries_for_finetune(sub_questions, all_subquestion_list, index_file, corpus_index, corpus, top_k=20)
-        # rand_neg_list = random_sample([corpus[i] for i in range(len(corpus)) if i not in reference_ids], 30)
+        top_20_retrieval = dense_retrieval_subqueries_for_finetune(sub_questions, all_subquestion_list, index_file, corpus_index, corpus, top_k=300)
+        rand_neg_list = random_sample([corpus[i] for i in range(len(corpus)) if i not in reference_ids], 30)
         negative_retrieval_set.update([negative_retrieval['chunk_id'] for negative_retrieval in top_20_retrieval if negative_retrieval['chunk_id'] not in reference_ids])
-        negative_retrieval_list = random_sample(list(negative_retrieval_set)[-100:],5)
+        negative_retrieval_list = random_sample(list(negative_retrieval_set)[200:300],5)
         negative_retrievals = [corpus[int(neg_id) - 1] for neg_id in negative_retrieval_list]
         assert len(negative_retrievals) >= 1, f"Not enough negative samples found for item: {item}"
         return negative_retrievals
@@ -476,14 +476,14 @@ def train(args, logger: logging.Logger):
             sim_pos = cos_sim(rep_anchor, rep_pos)
             sim_neg = torch.abs(cos_sim(rep_anchor, rep_neg))
 
-            losses = -(sim_pos - sim_neg)
+            losses = -sim_pos + sim_neg
             return losses.mean()
 
 
 
     train_loss = TripletLoss_self(model, triplet_margin=1)
 
-    train_loss = losses.MultipleNegativesRankingLoss(model,scale=20)
+    # train_loss = losses.MultipleNegativesRankingLoss(model,scale=20)
 
     logger.info(f"Training with {len(train_examples)} training examples and {len(test_examples)} test examples")
 
@@ -501,10 +501,10 @@ def train(args, logger: logging.Logger):
     model.fit(
         train_objectives=[(train_dataloader, train_loss)],
         epochs=args.epochs,
-        warmup_steps=2,
+        warmup_steps=100,
         evaluator=evaluator,
-        evaluation_steps=5,
-        optimizer_params= { "lr": 2e-5 },
+        evaluation_steps=100,
+        optimizer_params= { "lr": 2e-6 },
         save_best_model=True,
         show_progress_bar=True,
         use_amp=True,
